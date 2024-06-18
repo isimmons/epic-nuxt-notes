79ff7a0448e2ae164ad8a310609a12c668e25a87
Ian <isimmons33@gmail.com>
2024-06-17 20:50:38 -0400

    __nuxt div styles

--- app.vue
+++ app.vue
@@ -1,23 +1,5 @@
 <template>
   <NuxtLayout>
-  <Head>
-    <Title>foobar</Title>
-    <Meta name="description" content="fochizzle" />
-    <Link rel="icon" type="image/svg+xml" :href="favicon" sizes="any" />
-  </Head>
     <NuxtPage />
   </NuxtLayout>
 </template>
-
-<script setup>
-import favicon from "~/assets/favicon.svg";
-
-useHead({
-  bodyAttrs: {
-    class: "flex h-screen flex-col justify-between bg-background text-foreground",
-  },
-  // htmlAttrs: {
-  //   class: "h-screen overflow-x-hidden",
-  // },
-});
-</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,40 @@
+<script setup type="ts">
+import favicon from "~/assets/favicon.svg";
+
+useHead({
+  htmlAttrs: {
+    class: "h-full overflow-x-hidden",
+  },
+  bodyAttrs: {
+    class: "flex h-full flex-col justify-between bg-background text-foreground",
+  },
+});
+</script>
+
 <template>
-  <div>
-    <header class="container h-[100px] py-6 mx-auto">
-      <nav class="flex justify-between">
-        <NuxtLink to="/">
-          <div class="font-light">epic</div>
-          <div class="font-bold">notes</div>
-        </NuxtLink>
-      </nav>
-    </header>
-
-    <div class="flex min-h-[calc(100vh-148px)] "><slot /></div>
-
-    <div class="container flex justify-between mx-auto">
+  <Head>
+    <Title>foobar</Title>
+    <Meta name="description" content="fochizzle" />
+    <Link rel="icon" type="image/svg+xml" :href="favicon" sizes="any" />
+  </Head>
+  
+  <header class="container py-6 mx-auto">
+    <nav class="flex justify-between">
       <NuxtLink to="/">
         <div class="font-light">epic</div>
         <div class="font-bold">notes</div>
       </NuxtLink>
-      <p>Built with ♥️ by</p>
-    </div>
+    </nav>
+  </header>
+
+  <div class="flex-1 h-[calc(100vh-244px)]"><slot /></div>
+
+  <div class="container absolute bottom-0 flex justify-between mx-auto">
+    <NuxtLink to="/">
+      <div class="font-light">epic</div>
+      <div class="font-bold">notes</div>
+    </NuxtLink>
+    <p>Built with ♥️ by</p>
   </div>
+  
 </template>
